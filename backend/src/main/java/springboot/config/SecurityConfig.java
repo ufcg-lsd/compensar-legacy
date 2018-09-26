f7b2c9c1b4dd52eb8d61236bb2777f9e68c2be9f
marcelovitorino <marcelo.vitorino@ccc.ufcg.edu.br>
2018-09-26 08:26:58 -0300

    configuração dos paths que precisam de autenticação atualizada

--- backend/src/main/java/springboot/config/SecurityConfig.java
+++ backend/src/main/java/springboot/config/SecurityConfig.java
@@ -1,102 +1,60 @@
 package springboot.config;
 
-import javax.sql.DataSource;
+import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
+@Configuration
 @EnableWebSecurity
-@EnableAuthorizationServer
-@EnableResourceServer
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	
-
+	@Autowired
+	private Environment env;
 	
-	/**
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder;
+	
+	
+	private final String[] PUBLIC_MATCHERS_GET = {
+			"/h2-console/**"
+	};
+	
 	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		 String password = passwordEncoder().encode("123");
-		 auth.inMemoryAuthentication().withUser("marcelo").password(password).roles("ADMIN");
-	}
-	
-	@Bean
-	@Override
-	protected AuthenticationManager authenticationManager() throws Exception {
-		return super.authenticationManager();
-	}
-	*/
-    @Override
-    public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/resources/**");
-    }
-    
-    
-	  @Override
-	  public void configure(AuthenticationManagerBuilder builder) throws Exception {
-		 String password = passwordEncoder().encode("123");
-	    builder
-	        .inMemoryAuthentication()
-	        .withUser("marcelo").password(password)
-	            .roles("ADMIN");
-	  }
-	
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-	    return new BCryptPasswordEncoder();
-	}
-	
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/").permitAll().antMatchers("/api/usuario")
-            .hasAnyRole("ADMIN").anyRequest().authenticated().and().formLogin()
-            .permitAll().and().logout().permitAll();
+	protected void configure(HttpSecurity http) throws Exception{
 
-        http.csrf().disable();
-    }
-	
-	/**
-	  @Override
-	  protected void configure(HttpSecurity http) throws Exception {
-		  http
-		    .authorizeRequests()
-		        //.antMatchers("/resources/**", "/webjars/**").permitAll()
-		        .anyRequest().authenticated()
-		    .and()
-		    .httpBasic();
-	  }
-			
-			
-
-	@Bean
-	@Override
-	protected AuthenticationManager authenticationManager() throws Exception {
-		return super.authenticationManager();
-	}
-
-
-		@Override
-		protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-			 String password = passwordEncoder().encode("123");
-			auth.inMemoryAuthentication().withUser("zico").password(password).roles("ADMIN");
+		if(Arrays.asList(env.getActiveProfiles()).contains("test")) {
+			http.headers().frameOptions().disable();			
 		}
-	
-		@Bean
-		public PasswordEncoder passwordEncoder() {
-		    return new BCryptPasswordEncoder();
-		}
+		http.csrf().disable();
 		
-		*/
+
+		http.authorizeRequests()
+			.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
+			.anyRequest().authenticated()
+			.and().httpBasic();
+	}
+		
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+		String username = "aepc";
+		String password = "123";
+		
+		String passwordEncoded = this.bCryptPasswordEncoder().encode(password);
+		auth.inMemoryAuthentication().passwordEncoder(passwordEncoder)
+			.withUser(username).password(passwordEncoded).roles("USER", "ADMIN");
+	}
+	
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 }
