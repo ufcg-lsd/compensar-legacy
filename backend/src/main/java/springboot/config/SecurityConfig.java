6cfe09e016e7170dfc99bc7515a42cd7d613268c
marcelovitorino <marcelo.vitorino@ccc.ufcg.edu.br>
2018-10-02 07:51:20 -0300

    adição de permissão read_only

--- backend/src/main/java/springboot/config/SecurityConfig.java
+++ backend/src/main/java/springboot/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package springboot.config;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -22,9 +23,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable();
 
-		http.authorizeRequests().antMatchers("/api/questaoCompetencia/**").hasRole("ALL")
+		http.authorizeRequests().antMatchers("/api/**").hasRole("ALL")
 				.antMatchers("/api/alternativa/**", "/api/questaoObj/**", "/api/questaoSubj/**")
-				.hasAnyRole("ALL", "CRIA_QUESTOES").antMatchers("/api/usuario/**").hasAnyRole("ALL", "CRIA_QUESTOES")
+				.hasAnyRole("CRIA_QUESTOES").antMatchers("/api/usuario/**").hasAnyRole("CRIA_QUESTOES","RESPONDE_QUESTOES")
+				.antMatchers(HttpMethod.GET, "/api/alternativa/**", "/api/questaoObj/**", "/api/questaoSubj/**")
+				.hasRole("READ_ONLY")
 				.anyRequest().authenticated().and().httpBasic();
 	}
 
--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -11,19 +11,27 @@ grant all on db_aepc_test.* to 'marcelovitorino'@'localhost';
 
 -- Cria Enum de Competencias
 ALTER TABLE questao_competencia MODIFY competencia ENUM('coleta','paralelização','analise','representacao','decomposicao','abstracao','simulacao','automacao');
+
+
 -- Cria Enum de Permissao
-ALTER TABLE usuario_permissao MODIFY permissao ENUM('ALL','RESPONDE_QUESTOES','CRIA_QUESTOES');
+
+-- ALL -> Representa o papel de um gerenciador do sistema
+-- RESPONDE_QUESTOES -> Representa o papel de um aluno
+-- CRIA_QUESTOES -> Representa o papel de um professor
+-- READ_ONLY -> Representa o papel de um leitor da aplicação, não necessariamente cadastrado
+ALTER TABLE usuario_permissao MODIFY permissao ENUM('ALL','RESPONDE_QUESTOES','CRIA_QUESTOES','READ_ONLY');
 
 
 -- [Exemplos] 
 
 -- Cria usuarios para testes.
-insert into Usuario (id, nome,nome_instituicao,email, senha, ativo) values (1, 'marcelo', 'UFCG','marcelo@gmail.com', '$2a$10$ARppQC0FRWaGP4pnZqYbpuVyYOWIp4q1r2ViT3PGYK6BafD5PXFiS', true);
+insert into usuario (id, nome,nome_instituicao,email, senha, ativo) values (1, 'marcelo', 'UFCG','marcelo@gmail.com', '$2a$10$ARppQC0FRWaGP4pnZqYbpuVyYOWIp4q1r2ViT3PGYK6BafD5PXFiS', true);
 insert into Usuario (id, nome,nome_instituicao,email, senha, ativo) values (2, 'erick', 'UFCG','erick@gmail.com', '$2a$10$ARppQC0FRWaGP4pnZqYbpuVyYOWIp4q1r2ViT3PGYK6BafD5PXFiS', true);
 insert into Usuario (id, nome,nome_instituicao,email, senha, ativo) values (3, 'prof', 'UFCG','prof@gmail.com', '$2a$10$ARppQC0FRWaGP4pnZqYbpuVyYOWIp4q1r2ViT3PGYK6BafD5PXFiS', true);
 
+
 -- Adiciona permissão para um usuario.
-insert into Usuario_Permissao (email, permissao) values ('marcelo@gmail.com', 'ALL');
+insert into usuario_permissao (email, permissao) values ('marcelo@gmail.com', 'ALL');
 insert into Usuario_Permissao (email, permissao) values ('erick@gmail.com', 'RESPONDE_QUESTOES');
 insert into Usuario_Permissao (email, permissao) values ('prof@gmail.com', 'CRIA_QUESTOES');
 
