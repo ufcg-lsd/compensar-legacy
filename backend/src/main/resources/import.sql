df9892961756da8bd804fe3754c3832027af9086
marcelovitorino <marcelo.vitorino@ccc.ufcg.edu.br>
2018-11-05 17:41:30 -0300

    refactor no import.sql para adequação ao mongodb

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -3,25 +3,9 @@
 -- BD é criado automaticamente nos arquivos de execução.
 
 
--- Cria Enum de Competencias
-ALTER TABLE questao_competencia MODIFY competencia ENUM('coleta','paralelização','analise','representacao','decomposicao','abstracao','simulacao','automacao');
-
-
--- Cria Enum de Permissao
-
--- ALL -> Representa o papel de um gerenciador do sistema
--- RESPONDE_QUESTOES -> Representa o papel de um aluno
--- CRIA_QUESTOES -> Representa o papel de um professor
--- READ_ONLY -> Representa o papel de um leitor da aplicação, não necessariamente cadastrado
-ALTER TABLE usuario_permissao MODIFY permissao ENUM('ALL','RESPONDE_QUESTOES','CRIA_QUESTOES','READ_ONLY');
-
-
--- [Exemplos] 
+-- [Exemplos para popular] 
 
 -- Cria usuarios para testes.
-insert into usuario (id, nome,nome_instituicao,email, senha, ativo) values (1, 'marcelo', 'UFCG','marcelo@gmail.com', '$2a$10$ARppQC0FRWaGP4pnZqYbpuVyYOWIp4q1r2ViT3PGYK6BafD5PXFiS', true);
-insert into usuario (id, nome,nome_instituicao,email, senha, ativo) values (2, 'erick', 'UFCG','erick@gmail.com', '$2a$10$ARppQC0FRWaGP4pnZqYbpuVyYOWIp4q1r2ViT3PGYK6BafD5PXFiS', true);
-insert into usuario (id, nome,nome_instituicao,email, senha, ativo) values (3, 'prof', 'UFCG','prof@gmail.com', '$2a$10$ARppQC0FRWaGP4pnZqYbpuVyYOWIp4q1r2ViT3PGYK6BafD5PXFiS', true);
 
 db.usuario.insert({
     "_id" : "marcelo@gmail.com",
@@ -32,13 +16,31 @@ db.usuario.insert({
     "ativo" : true
 })
 
+db.usuario.insert({
+    "_id" : "erick@gmail.com",
+    "nome" : "erick",
+    "nomeInstituicao" : "ufcg",
+    "senha" : "$2a$10$88JUBC8rMyoFe0M.kzifweoZsnrKemsdPXbKYUGn53V4KlbxAwwZ6",
+    "email" : "erick@gmail.com",
+    "ativo" : true
+})
+
+db.usuario.insert({
+    "_id" : "prof@gmail.com",
+    "nome" : "prof",
+    "nomeInstituicao" : "ufcg",
+    "senha" : "$2a$10$88JUBC8rMyoFe0M.kzifweoZsnrKemsdPXbKYUGn53V4KlbxAwwZ6",
+    "email" : "prof@gmail.com",
+    "ativo" : true
+})
 
 
-
--- Adiciona permissão para um usuario.
-insert into usuario_permissao (email, permissao) values ('marcelo@gmail.com', 'ALL');
-insert into usuario_permissao (email, permissao) values ('erick@gmail.com', 'RESPONDE_QUESTOES');
-insert into usuario_permissao (email, permissao) values ('prof@gmail.com', 'CRIA_QUESTOES');
+-- Tipos de permissões aceitas
+------
+-- ALL -> Representa o papel de um gerenciador do sistema
+-- RESPONDE_QUESTOES -> Representa o papel de um aluno
+-- CRIA_QUESTOES -> Representa o papel de um professor
+-- READ_ONLY -> Representa o papel de um leitor da aplicação, não necessariamente cadastrado
 
 db.usuario_permissao.insert({
     "_id" : "marcelo@gmail.com",
@@ -46,11 +48,18 @@ db.usuario_permissao.insert({
     "permissao" : "ALL"
 })
 
+db.usuario_permissao.insert({
+    "_id" : "erick@gmail.com",
+    "email" : "erick@gmail.com",
+    "permissao" : "RESPONDE_QUESTOES"
+})
 
--- Adição de questões
+db.usuario_permissao.insert({
+    "_id" : "prof@gmail.com",
+    "email" : "prof@gmail.com",
+    "permissao" : "CRIA_QUESTOES"
+})
 
-insert into questao (autor,enunciado,fonte,imagem,tipo) values ('marcelo@gmail.com','Quanto é o triplo do quadrado de 10','PISA',null,'subj');
-insert into quest_subj (autor,enunciado,fonte,imagem,tipo) values ('marcelo@gmail.com','Quanto é o triplo do quadrado de 10','PISA',null,'subj');
 
 
 
