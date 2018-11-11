87ca209261ed5e859916eb3b60aca435ea5247e2
marcelovitorino <marcelo.vitorino@ccc.ufcg.edu.br>
2018-11-11 00:05:33 -0300

    tela inicial funcionando

--- frontend/app/app.js
+++ frontend/app/app.js
@@ -1,16 +1,37 @@
-// When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
+//Ao rolar a página para baixo diminui tamanho da navbar
+
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
     const navbar =  document.getElementById("navbar");
-    const logo =  document.getElementById("logo");
 
 
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-    navbar.style.padding = "30px 10px";
-    logo.style.fontSize = "25px";
+    navbar.style.padding = "20px 10px";
   } else {
-    navbar.style.padding = "80px 10px";
-    logo.style.fontSize = "35px";
+    navbar.style.padding = "50px 10px";
   }
 }
+
+//Desliza icones automaticamente sem desordenar
+
+$('#carouselExample').on('slide.bs.carousel', function (e) {
+
+  var $e = $(e.relatedTarget);
+  var idx = $e.index();
+  var itemsPerSlide = 4;
+  var totalItems = $('.carousel-item').length;
+  
+  if (idx >= totalItems-(itemsPerSlide-1)) {
+      var it = itemsPerSlide - (totalItems - idx);
+      for (var i=0; i<it; i++) {
+          // append slides to end
+          if (e.direction=="left") {
+              $('.carousel-item').eq(i).appendTo('.carousel-inner');
+          }
+          else {
+              $('.carousel-item').eq(0).appendTo('.carousel-inner');
+          }
+      }
+  }
+});
