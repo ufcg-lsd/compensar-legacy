bb62d95056f24a5e507bbf876fbbc599526aad6d
marcelovitorino <marcelo.vitorino@ccc.ufcg.edu.br>
2019-07-08 18:33:39 +0200

    ordenando mapeamento de chaves no enum de competencias

--- backend/src/main/java/springboot/enums/CompetenciaType.java
+++ backend/src/main/java/springboot/enums/CompetenciaType.java
@@ -2,14 +2,14 @@ package springboot.enums;
 
 public enum CompetenciaType {
 	COMP_COLETA("collect"),
-	COMP_PARALELIZAÇÃO("analysis"),
-	COMP_ANÁLISE("representation"),
-	COMP_REPRESENTAÇÃO("decomposition"),
-	COMP_DECOMPOSIÇÃO("algorithms"),
+	COMP_PARALELIZAÇÃO("parallelization"),
+	COMP_ANÁLISE("analysis"),
+	COMP_REPRESENTAÇÃO("representation"),
+	COMP_DECOMPOSIÇÃO("decomposition"),
 	COMP_ABSTRAÇÃO("abstraction"),
-	COMP_SIMULAÇÃO("automation"),
-	COMP_AUTOMAÇÃO("parallelization"),
-	COMP_ALGORITMOS("simulation");
+	COMP_SIMULAÇÃO("simulation"),
+	COMP_AUTOMAÇÃO("automation"),
+	COMP_ALGORITMOS("algorithms");
 	
 	
 	public final String value;
