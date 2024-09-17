5e89495620ea9382d051f649e9c77ea05587d7de
João Vitor <joao.luciano@lsd.ufcg.edu.br>
2024-09-17 02:39:04 -0300

    clean exception files

--- backend/src/main/java/springboot/exception/data/PermissionDeniedException.java
+++ backend/src/main/java/springboot/exception/data/PermissionDeniedException.java
@@ -1,7 +1,9 @@
 package springboot.exception.data;
 
 public class PermissionDeniedException extends RuntimeException {
-    public PermissionDeniedException(String message) {
+    private static final long serialVersionUID = 2035267621092903695L;
+
+	public PermissionDeniedException(String message) {
         super(message);
     }
 }
--- backend/src/main/java/springboot/exception/data/RegisterNotFoundException.java
+++ backend/src/main/java/springboot/exception/data/RegisterNotFoundException.java
@@ -1,6 +1,8 @@
 package springboot.exception.data;
 
 public class RegisterNotFoundException extends RuntimeException {
+	private static final long serialVersionUID = 7218176289464335813L;
+
 	public RegisterNotFoundException(String message) {
 		super(message);
 	}
